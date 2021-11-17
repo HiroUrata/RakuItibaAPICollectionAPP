6cd764e4233c07ba1833e31b4424f48480a42224
HiroUrata <1712026@i.softbank.jp>
2021-11-17 23:40:17 +0900

    ResultItemListCell 作成

---
+++ RakuItibaAPICollectionAPP/ResultItemListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResultItemListCell.swift
+//  RakuItibaAPICollectionAPP
+//
+//  Created by UrataHiroki on 2021/11/17.
+//
+
+import UIKit
+
+class ResultItemListCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var mediumImageView: UIImageView!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    @IBOutlet weak var shopNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- RakuItibaAPICollectionAPP/ViewController.swift
+++ RakuItibaAPICollectionAPP/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
