7ae18e339f81a95371ad0d15fdf2cc56ced702a1
HiroUrata <1712026@i.softbank.jp>
2021-11-18 16:49:06 +0900

    ItemResultDetailModel 追加

---
+++ RakuItibaAPICollectionAPP/Model/ItemResultDetailModel.swift
@@ -0,0 +1,15 @@
+//
+//  ItemResultDetailModel.swift
+//  RakuItibaAPICollectionAPP
+//
+//  Created by UrataHiroki on 2021/11/18.
+//
+
+
+struct ItemResultDetailModel{
+    
+    let mediumImageUrl:String?
+    let itemName:String?
+    let itemPrice:Int?
+    let shopName:String?
+}
