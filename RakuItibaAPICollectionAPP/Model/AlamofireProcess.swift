591d2e59a2f2f9307b1726b4232f1e9f2704131f
HiroUrata <1712026@i.softbank.jp>
2021-11-18 17:14:20 +0900

    AlamofireProcess  getItemDetailData() 作成

--- RakuItibaAPICollectionAPP/Model/AlamofireProcess.swift
+++ RakuItibaAPICollectionAPP/Model/AlamofireProcess.swift
@@ -10,7 +10,7 @@ import SwiftyJSON
 
 class AlamofireProcess{
     
-    private var ResultDetailDatas = [ItemResultDetailModel]()
+    private var resultDetailDatas = [ItemResultDetailModel]()
 }
 
 extension AlamofireProcess{
@@ -20,6 +20,37 @@ extension AlamofireProcess{
         guard let key = searchKey else { return }
         
         let apiKey = ""
+        
+        AF.request(apiKey, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON {[self] response in
+            
+            switch response.result{
+                
+            case .success:
+                
+                let getdata = JSON(response.data as Any)
+                resultDetailDatas = []
+                
+                for dataCount in 0..<getdata["Items"].count{
+                    
+                    if let getMediumImageUrl = getdata["Items"][dataCount]["Item"]["mediumImageUrls"][0].string,
+                       let getItemName = getdata["Items"][dataCount]["Item"]["itemName"].string,
+                       let getItemPrice = getdata["Items"][dataCount]["Item"]["itemPrice"].int,
+                       let getShopName =  getdata["Items"][dataCount]["shopName"].string{
+                        
+                        resultDetailDatas.append(ItemResultDetailModel(mediumImageUrl: getMediumImageUrl,
+                                                                       itemName: getItemName,
+                                                                       itemPrice: getItemPrice,
+                                                                       shopName: getShopName))
+                    }
+                    
+                }
+                completion(resultDetailDatas, nil)
+                
+            case .failure(let error):
+                
+                completion(nil, error)
+            }
+        }
         
     }
 }
